2dbb2669824760a4d4832b220477e012bf1374a5
Victor Morales <u0512418@ad.utah.edu>
2019-08-09 09:27:11 -0600

    Init User and service user change password

--- ChangeWinServiceUser.ps1
+++ ChangeWinServiceUser.ps1
@@ -1,50 +1,119 @@
+#Getting the list
 $serverList = Get-FilePath
 $list = Get-Content $serverList
 
-$user = read-host "Enter the name of the user you want to change (DOMAIN\User)"
-$pass = read-host -assecurestring "New Password" | convertfrom-securestring | ConvertTo-SecureString
-
-
-
+#the user to change
+$user = read-host "Enter the name of the user you want to change"
+$password = read-host -assecurestring "New Password" | convertfrom-securestring | ConvertTo-SecureString
 
 if($list -match "localhost"){
-script -server $list -user $user -pass $pass
-}
-else{
-  $mycred = Get-Credential -Message "Enter creds to log into the servers"
-  $list | Invoke-Command -ComputerName $_ -Credential $mycred -scriptblock {script -server $_ -user $user -pass $pass}
-}
+        changeWinServiceAccount -winservice $winService -user $user -pass $password
+  }
+  else{
+      #executing remotely
+      $mycred = Get-Credential -Message "Enter creds to log into the servers"
+      $list | %{
+                try{ 
+                    Invoke-Command -ComputerName $_ -Credential $mycred -scriptblock {
+   
+                     
+                        Function changeWinServiceAccount{
+                            param (
+                                $winService, $user, $password            
+                            )
+                            if((get-Service) -match $winService){
+                                try{
+                                    sc.exe config $winService obj= $user password= $password
+                                    $message = "pass"    
+                                    return $message                                    
+                                        }
+                                catch{
+                                    $message = $_.Exception.Message
+                                    return  $message
+                                }
+                            }
+                            else{
+                                $message = "Service not found"
+                                return  $message
+                            }
+                    }
+
+                        $user = $using:user
+                        $password = $using:password
+                        $winservice = $using:winService
+
+                        changeWinServiceAccount -winservice $winService -user $user -pass $password    
+
+                    }
+                    return appendToFile -computerName $_ -message $message -fileLocation C:\Users\$env:USERNAME\Desktop\changeWinServiceAccount.txt  
+                }
+                catch {
+                    appendToFile -computerName $_ -message "failed to connect" -fileLocation C:\Users\$env:USERNAME\Desktop\changeWinServiceAccount.txt
+                }           
+            }
+    }    
+    
 
 
-function script{
-    param($server,$user,$pass)
-    function changeWinServiceUser {
-        param ($server,$user,$pass)
-        $winService = ""
-        try{
-        sc.exe config $winService obj= $user password= $pass
-        appendToFile -computerName $server -message "Changed"   
-        }
-        catch{
-            appendToFile -computerName $server -message 
-        }
 
-    }
-
-
-    function appendToFile{
-        param(
-            $computerName, $message
+Function changeWinServiceAccount{
+        param (
+            $winService, $user, $password            
         )
-        $fileLocation = "C:\Users\$env:USERNAME\Desktop\passwordChange.txt"
+        if((get-Service) -match $winService){
+            try{
+                sc.exe config $winService obj= $user password= $password
+                $message = "pass"    
+                return $message                                    
+                    }
+            catch{
+                $message = $_.Exception.Message
+                return  $message
+            }
+        }
+        else{
+            $message = "Service not found"
+            return  $message
+        }
+}
+
+
+
+
+
+
+Function Get-FilePath{ 
+    try{
+            
+        #loading module for windows forums            
+        [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
+        Out-Null
+        #creating new object for opening file dialog
+        $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+        $OpenFileDialog.filter = "All files (*.*)| *.*"
+        $OpenFileDialog.ShowDialog() | Out-Null
+        $OpenFileDialog.filename
+    }
+    Catch{
+            Write-Host "ERROR: $($Error[0])";
+        }
+        
+    }
+Function appendToFile{
+        param(
+            $computerName, $message, $fileLocation
+        )
         $date= get-date | Select -ExpandProperty DateTime
         $appendMessage = @($computerName," ",$message," ",$date)
         $appendMessage | Out-File $fileLocation -NoNewline -Append
         "`n" | Out-File $fileLocation -Append
-
-
+    
+    
     }
 
 
-changeWinServiceUser -server $server -user $user -pass $pass    
-}
+
+
+
+
+        
--- PasswordChange.ps1
+++ PasswordChange.ps1
@@ -1,63 +1,7 @@
 
-function script{
-    param ($server, $user, $pass)
-
-    function invokeChange {
-        param (
-            $serverName, $fileLocation, $user, $password
-        )
-        if((test-connection $serverName -Quiet).Equals($True)){
-                if((net user) -match $user){
-                        try{
-                            net user $user $password
-                            appendToFile -computerName $serverName -message "Pass" -fileLocation $fileLocation
-                        }
-                        catch{
-                            appendToFile -computerName $serverName -message $_.Exception.Message -fileLocation $fileLocation
-                        }
-                        }
-                else{
-                        appendToFile -computerName $serverName -message "User not found" -fileLocation $fileLocation
-                }
-            
-        }
-        else{
-            appendToFile -computerName $serverName -message "Fail to connect" -fileLocation $fileLocation
-    
-            
-            }
-        }
-    
-    
-    
-    
-    function appendToFile{
-        param(
-            $computerName, $message, $fileLocation
-        )
-        $date= get-date | Select -ExpandProperty DateTime
-        $appendMessage = @($computerName," ",$message," ",$date)
-        $appendMessage | Out-File $fileLocation -NoNewline -Append
-        "`n" | Out-File $fileLocation -Append
-    
-    
-    }
-    
-
-    
-     
- 
-
-    
-    invokeChange -serverName $server -fileLocation "C:\Users\$env:USERNAME\Desktop\passwordChange.txt" -user $user -password $pass
-}
-
-
-
 Function Get-FilePath{ 
-    try{
-            
-            
+    try{            
+        #loading module for windows forums            
         [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
         Out-Null
         #creating new object for opening file dialog
@@ -71,31 +15,100 @@ Function Get-FilePath{
         }
         
     }
+Function appendToFile{
+        param(
+            $computerName, $message, $fileLocation
+        )
+        $date= get-date | Select -ExpandProperty DateTime
+        $appendMessage = @($computerName," ",$message," ",$date)
+        $appendMessage | Out-File $fileLocation -NoNewline -Append
+        "`n" | Out-File $fileLocation -Append
+    
+    
+    }
+
+Function changePassword{
+        param (
+            $user, $password
+        )
+        if((net user) -match $user){
+            try{
+                net user $user $password
+                $message = "pass"    
+                return $message                                    
+                    }
+            catch{
+                $message = $_.Exception.Message
+                return  $message
+            }
+        }
+        else{
+            $message = "User not found"
+            return  $message
+        }
+    }
 
 
-    
-    $serverList = Get-FilePath
-    $list = Get-Content $serverList
- 
-    
-    $user = read-host "Enter the name of the user you want to change"
-    $pass = read-host -assecurestring "New Password" | convertfrom-securestring | ConvertTo-SecureString
+
+
+
+
+
+#Getting the list
+$serverList = Get-FilePath
+$list = Get-Content $serverList
+
+#the user to change
+$user = read-host "Enter the name of the user you want to change"
+$password = read-host -assecurestring "New Password" | convertfrom-securestring | ConvertTo-SecureString
     
   
 
 
   if($list -match "localhost"){
-    script -server $list -user $user -pass $pass
-    Write-Host $list
-    
+    changePassword -user $user -pass $password
   }
   else{
+      #executing remotely
       $mycred = Get-Credential -Message "Enter creds to log into the servers"
+      $list | %{
+                try{ 
+                    Invoke-Command -ComputerName $_ -Credential $mycred -scriptblock {
    
-    
-      $list | %{Invoke-Command -ComputerName $_ -Credential $mycred {script -server $_ -user $user -pass $pass}}
-  }
-  
-    
+                     
+                        function changePassword{
+                            param (
+                                $user, $password
+                            )
+                            if((net user) -match $user){
+                                try{
+                                    net user $user $password
+                                    write-host $user
+                                                              
+                                        }
+                                catch{
+                                    $message = $_.Exception.Message
+                                    return  $message
+                                }
+                            }
+                            else{
+                                $message = "User not found"
+                                return  $message
+                            }
+                        }
 
+                        $user = $using:user
+                        $password = $using:password
+
+                        changePassword -user $user -pass $password    
+
+                    }
+                    return appendToFile -computerName $_ -message $message -fileLocation C:\Users\$env:USERNAME\Desktop\changePassword.txt  
+                }
+                catch {
+                    appendToFile -computerName $_ -message "failed to connect" -fileLocation C:\Users\$env:USERNAME\Desktop\changePassword.txt
+                }           
+            }
+    }    
     
+        
